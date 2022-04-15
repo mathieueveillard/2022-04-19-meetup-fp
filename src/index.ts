1a6cea30e443e21db25e61e7891d3a144574dc04
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-04-15 16:06:24 +0200

    Get rid of side causes (step 2)

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 const isMultipleOf = (m: number, n: number): boolean => n % m === 0;
 
+const appendFizzIfNIsMultipleOf3 = (n: number, s: string): string => {
+  if (isMultipleOf(3, n)) {
+    return s + "Fizz";
+  }
+  return s;
+};
+
+const appendBuzzIfNIsMultipleOf5 = (n: number, s: string): string => {
+  if (isMultipleOf(5, n)) {
+    return s + "Buzz";
+  }
+  return s;
+};
+
+const appendNOtherwise = (n: number, s: string): string => {
+  return s || n.toString();
+};
+
 export const replace = (n: number): string => {
-  const appendFizzIfNIsMultipleOf3 = (s: string): string => {
-    if (isMultipleOf(3, n)) {
-      return s + "Fizz";
-    }
-    return s;
-  };
-
-  const appendBuzzIfNIsMultipleOf5 = (s: string): string => {
-    if (isMultipleOf(5, n)) {
-      return s + "Buzz";
-    }
-    return s;
-  };
-
-  const appendNOtherwise = (s: string): string => {
-    return s || n.toString();
-  };
-
   const step0 = "";
-  const step1 = appendFizzIfNIsMultipleOf3(step0);
-  const step2 = appendBuzzIfNIsMultipleOf5(step1);
-  const step3 = appendNOtherwise(step2);
+  const step1 = appendFizzIfNIsMultipleOf3(n, step0);
+  const step2 = appendBuzzIfNIsMultipleOf5(n, step1);
+  const step3 = appendNOtherwise(n, step2);
   return step3;
 };
 
